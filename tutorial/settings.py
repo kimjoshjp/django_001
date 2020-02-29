cb22417fee9298d6dd0c7630e4be56ede045cfac
kimjoshjp <kimjoshjp@gmail.com>
2020-02-29 10:25:46 +1000

    Update settings.py
    Switch back to False from True

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z#_0#6#5lzv&*q44h#b*gazq-5ul3k%p#*@a&q-9^woj6kj1*i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
